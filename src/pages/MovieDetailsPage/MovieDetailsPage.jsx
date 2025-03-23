b2981060e3db6ce925fd5e80a4852308020d1e69
Tatyana25081974 <tatyanakorabelnikovamama@gmail.com>
2025-03-23 21:53:14 +0200

    fix

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -24,30 +24,33 @@ const backLink = location.state?.from || '/movies';
   }, [movieId]);
 
   if (!movie) return <p>Завантаження...</p>;
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : 'https://via.placeholder.com/300x450?text=No+Image';
 
   return (
-  <div className={css.wrapper}>
-    <BackButton />
-    <h2 className={css.title}>{movie.title}</h2>
-    <p className={css.rating}>Рейтинг: {movie.vote_average}</p>
-    <p className={css.overview}>Опис: {movie.overview}</p>
-    <p className={css.genres}>
-      Жанри: {movie.genres.map(genre => genre.name).join(', ')}
+    <div className={css.wrapper}>
+      <BackButton />
+      <img className={css.poster} src={posterUrl} alt={movie.title} />
+      <h2 className={css.title}>{movie.title}</h2>
+      <p className={css.rating}>Рейтинг: {movie.vote_average}</p>
+      <p className={css.overview}>Опис: {movie.overview}</p>
+      <p className={css.genres}>
+        Жанри: {movie.genres.map(genre => genre.name).join(', ')}
       </p>
       <hr />
-<p className={css.subTitle}>Додаткова інформація:</p>
-<ul className={css.subLinks}>
-  <li>
-    <Link to="cast">Cast</Link>
-  </li>
-  <li>
-    <Link to="reviews">Reviews</Link>
-  </li>
-</ul>
-
-<Suspense fallback={<p>Завантаження...</p>}>
-  <Outlet />
-</Suspense>
-  </div>
-);
+      <p className={css.subTitle}>Додаткова інформація:</p>
+      <ul className={css.subLinks}>
+        <li>
+          <Link to="cast" state={{ from: backLink }}>Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews" state={{ from: backLink }}>Reviews</Link>
+        </li>
+      </ul>
+      <Suspense fallback={<p>Завантаження...</p>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 }
