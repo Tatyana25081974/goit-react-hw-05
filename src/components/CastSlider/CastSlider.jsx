0d80392d035f56cb1befc42c4d1e4d791eb352ea
Tatyana25081974 <tatyanakorabelnikovamama@gmail.com>
2025-03-24 18:51:29 +0200

    fix

--- src/components/CastSlider/CastSlider.jsx
+++ src/components/CastSlider/CastSlider.jsx
@@ -1,22 +1,35 @@
+// CastSlider.jsx
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
-import './CastSlider.css';
+import './CastSlider.css'; // Підключення стилів
 
 export const CastSlider = ({ cast }) => {
   return (
     <Swiper
       modules={[Autoplay]}
       spaceBetween={20}
-      slidesPerView={6}             
-      slidesPerGroup={1}           
       loop={true}
       autoplay={{
-        delay: 0,                  
+        delay: 0, // постійна прокрутка
         disableOnInteraction: false,
       }}
-      speed={2000}                 
+      speed={2000} // плавність руху
       grabCursor={true}
+      breakpoints={{
+        0: {
+          slidesPerView: 2,
+        },
+        480: {
+          slidesPerView: 3,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 6,
+        },
+      }}
     >
       {cast.map(actor => (
         <SwiperSlide key={actor.id}>
